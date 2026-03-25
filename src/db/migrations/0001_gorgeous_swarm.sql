1464992b4c017ed41a2080056bd26a4a223777a3
もも ももか <90553519+MomokaSada@users.noreply.github.com>
2026-03-26 06:38:30 +0900

    Revert "[create] auth system and migration fix"

--- src/db/migrations/0001_gorgeous_swarm.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "edit_sessions" ALTER COLUMN "max_edits" SET NOT NULL;--> statement-breakpoint
-ALTER TABLE "edit_sessions" ADD COLUMN "edits_used" integer DEFAULT 0 NOT NULL;--> statement-breakpoint
-ALTER TABLE "edit_sessions" ADD COLUMN "revoked_at" timestamp;--> statement-breakpoint
-ALTER TABLE "edit_sessions" DROP COLUMN "is_active";--> statement-breakpoint
-ALTER TABLE "edit_sessions" DROP COLUMN "start_at";
